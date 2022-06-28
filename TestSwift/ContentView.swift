b5fa92c9cb1b8693faf441b7a66feed738164057
刘耀文 <287228558@qq.com>
2022-06-28 15:31:33 +0800

    1

---
+++ TestSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestSwift
+//
+//  Created by 刘耀文 on 2022/6/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!123")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestSwift/TestSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftApp.swift
+//  TestSwift
+//
+//  Created by 刘耀文 on 2022/6/27.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
